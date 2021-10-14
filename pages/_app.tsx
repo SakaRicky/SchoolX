1f48a5e276583302b79dad5299b41c961a6ad819
Saka Ricky <sakaricky91@gmail.com>
2021-10-14 17:41:42 +0100

    Setup: Added MaterialUI, Typescript, Sass, NextJS, so the project can start

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.scss'
+import { FC } from 'react';
+import type { AppProps } from 'next/app';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
