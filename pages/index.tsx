9ad5737b9f35ca38ff76171cba63246fed96e97a
Saka Ricky <sakaricky91@gmail.com>
2021-10-15 17:16:17 +0100

    fix: changed the import paths from relative to absolute

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '/Users/rickysaka/Desktop/Work_Desk/SchoolX/styles/Home.module.scss';
+import styles from '/styles/Home.module.scss';
 import { theme } from '../theme';
 import { ThemeProvider } from '@material-ui/styles';
 import { Typography, Button } from '@material-ui/core';
