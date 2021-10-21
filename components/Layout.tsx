74d802cf57b32176f25d205e3a3754598a1b27ad
Saka Ricky <sakaricky91@gmail.com>
2021-10-21 17:38:50 +0100

    chore: commiting before running codemod

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import {
+    AppBar,
+    CssBaseline,
+    Drawer,
+    Hidden,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Toolbar,
+    Typography,
+    Avatar,
+    ListItemIcon,
+    makeStyles
+
+} from '@material-ui/core';
+
 import MenuIcon from '@material-ui/icons/Menu';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
@@ -15,11 +24,9 @@ import FormatLineSpacingIcon from '@material-ui/icons/FormatLineSpacing';
 import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import CloseIcon from '@material-ui/icons/Close';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, ListItemIcon } from '@material-ui/core';
+
 import { stringAvatar } from 'utils'
+
 interface LayoutProps {
     children: React.ReactNode;
 }
