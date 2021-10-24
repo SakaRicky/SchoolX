9ab820def4ee67b146ff2248486007f360c2cf01
Saka Ricky <sakaricky91@gmail.com>
2021-10-24 15:03:48 +0100

    feat: imported the state and made new type

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import { Typography } from "@material-ui/core";
+import { useStateValue, setUser } from "state";
 
 const Dashboard = () => {
+    const [state, dispatch] = useStateValue()
+
+    console.log(state.user);
+    
+    useEffect(() => {
+        dispatch(setUser({
+            email: 'sakaricky91@gmail.com',
+            name: "Saka Ricky"
+        }))
+    }, [])
     return (
         <div>
             <Typography variant="h6">
