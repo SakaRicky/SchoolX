db1ad8baecb3e77b971cc2b0e2ff3a4a57bf6312
Saka Ricky <sakaricky91@gmail.com>
2021-10-27 15:10:28 +0100

    feat: refactored the service and created a route for marksentry

--- services/student.ts
+++ services/student.ts
@@ -4,13 +4,7 @@ import axios from "axios";
 
 const baseURL = process.env.HOST;
 
-const saveStudent = async (newStudent: NewStudent) => {
+export const saveStudent = async (newStudent: NewStudent) => {
     const res = await axios.post(`${baseURL}students`, newStudent)
     return res.data;
 }
-
-const studentServices = {
-    saveStudent
-}
-
-export default studentServices
